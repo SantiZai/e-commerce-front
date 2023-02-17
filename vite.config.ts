c6b354ea0e2cb569e5f030177a127d5e46725407
Santiago Zaidan <santyagozaidan@gmail.com>
2023-02-16 22:22:51 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    sourcemap: true,
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'src/index.tsx',
-      output: {
-        format: 'esm',
-        dir: 'dist'
-      },
-      external: ['react', 'react-dom'],
-      plugins: []
-    }
-  }
+  plugins: [react()],
 })
