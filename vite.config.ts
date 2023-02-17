0d10f8f36cc7e9be1fbfa9f7a2eb3257c0abd8f9
Santiago Zaidan <santyagozaidan@gmail.com>
2023-02-17 14:59:03 -0300

    gh pages added

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-    'src/**/*.ts',
-    'src/**/*.tsx',
-    'src/**/*.js',
-    'src/**/*.jsx',
-    'src/**/*.mjs'
-  ]
-  }
+  base: '/e-commerce-front/'
 })
